1b68fbc4e2f63767327d934de36198ccca065e2f
Nadan Marenković <nadan.marenkovic@gmail.com>
2022-08-29 13:53:50 +0200

    Add files via upload

---
+++ Zadatak1/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using Zadatak1.Models;
+
+namespace Zadatak1.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("api/events/create")]
+        public ActionResult Create([FromBody]XElement request) {       
+            if (!IsValidXml(new XDocument(request))) {
+                return BadRequest("Sent XML is not valid!");
+            }
+
+            return Ok();
+        }
+
+        private bool IsValidXml(XDocument doc) {
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", @"C:\Users\nadan.marenkovic\Desktop\tuts\ProjektniZadatakIIS\Zadatak1\Models\Event.xsd");
+            doc.Validate(schema, ValidationEventHandler);
+            return true;
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs e) {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ Zadatak2/Controllers/TicketsController.cs
@@ -0,0 +1,50 @@
+﻿using Commons.Xml.Relaxng;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Zadatak2.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("api/tickets/create")]
+        public ActionResult Create([FromBody] XElement request)
+        {
+            if (!IsValidXml(new XDocument(request)))
+            {
+                return BadRequest("Sent XML is not valid!");
+            }
+            return Ok();
+        }
+
+        private bool IsValidXml(XDocument doc) {
+            XmlReader xml = doc.CreateReader();
+            XmlReader rng = XmlReader.Create(@"C:\Users\nadan.marenkovic\Desktop\tuts\ProjektniZadatakIIS\Zadatak2\Model\Ticket.rng");
+
+            using (RelaxngValidatingReader reader = new RelaxngValidatingReader(xml, rng))
+            {
+                try
+                {
+                    while (!reader.EOF)
+                    {
+                        reader.Read();
+                    }
+                    Console.WriteLine("validation succeeded");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("validation failed with message:");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return true;
+        }
+    }
+}
